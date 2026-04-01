cb03930b1f55504bb965b43e5062dc741aea060b
Aziza <azizadalleji@gmail.com>
2026-04-01 17:46:22 +0100

    update

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'aziza',
-  database: process.env.DB_NAME || 'student_db',
+  host: process.env.MYSQLHOST ,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER ,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
